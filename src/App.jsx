09deb8b8c3f81f1549d9a87c42b6cc34be789aa5
TysonPWilliams <tyson.williams95@gmail.com>
2025-05-14 21:14:41 +1000

    Initialised and created React App

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import satoshiFund from './assets/satoshiFund.png';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="navbar">
+        <img src={satoshiFund} alt="logo" className='logo' />
+        <button className='login-btn'>Login</button>
+      </header>
+
+      <main className="main-content">
+        <div id='hero-text'>
+          <h1>Borrow or Lend Bitcoin Securely. Instantly.</h1>
+          <p>A decentralized peer-to-peer platform empowering users to lend or borrow Bitcoin without banks</p>
+        </div>
+        
+        <div id='landing-page-btns'>
+          <button className='btn btn-primary'>Borrow BTC</button>
+          <button className='btn btn-secondary'>Lend BTC</button>
+        </div>
+
+
+      </main>
+
+    </div>
+
+
+  )
+}
+
+export default App
