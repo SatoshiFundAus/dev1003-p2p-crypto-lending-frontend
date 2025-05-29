76393a5f798b29a2144c3dbc195ccafc110db3c4
TysonPWilliams <tyson.williams95@gmail.com>
2025-05-29 17:44:37 +1000

    WIP, started fleshing out Wallet component

---
+++ src/components/Wallet.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+
+
+function Wallet() {
+
+    const [userEmail, setUserEmail] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            try {
+                const tokenData = JSON.parse(atob(token.split('.')[1]));
+                setUserEmail(tokenData.email)
+            } catch (err) {
+                console.error('Error parsing token:', err);
+                setUserEmail('')
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoading(true);
+                setError(null);
+
+                // Get stored user data
+                const token = localStorage.getItem('token');
+                const userEmail = localStorage.getItem('userEmail')
+                const isAdmin = localStorage.getItem('isAdmin') === 'true';
+            } catch (err) {
+                console.error('An error has occured:', err)
+            }
+        }
+    })
+
+    return (
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    )
+
+};
+
+export default Wallet()
